0a6396419e4dc554c70b0a66857deadaa98da10f
wcv007 <varunsingla2007@gmail.com>
2020-02-07 17:13:45 +0530

    Updates

---
+++ precache-manifest.8cb0fe31250408fc1baeab6aa46d5049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53a04b4281e318a201d2d037f825e92",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ba06ad3448689ca89f5d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "71677199ea23270f6255",
+    "url": "/robofriends/static/css/main.f657a740.chunk.css"
+  },
+  {
+    "revision": "ba06ad3448689ca89f5d",
+    "url": "/robofriends/static/js/2.d195c58b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.d195c58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71677199ea23270f6255",
+    "url": "/robofriends/static/js/main.08c8a258.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
